3408ea0dd7e2805c13356f6a5e1b6e0585f367df
Guilherme Sá <guilsa001@gmail.com>
2015-05-12 21:39:25 -0700

    new seed

--- db/migrate/20140127173922_create_journals.rb
+++ db/migrate/20140127173922_create_journals.rb
@@ -2,6 +2,8 @@ class CreateJournals < ActiveRecord::Migration
   def change
     create_table(:journals) do |t|
       t.string :content
+      t.belongs_to :user
+
       t.timestamp
     end
   end
