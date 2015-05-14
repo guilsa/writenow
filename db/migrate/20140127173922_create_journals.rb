12ff3f6bbab9f31a59c5e50f6c8d21d2dcc6ad5c
Guilherme Sá <guilsa001@gmail.com>
2015-05-13 21:25:05 -0700

    db migration added goal and title column to Journals table

--- db/migrate/20140127173922_create_journals.rb
+++ db/migrate/20140127173922_create_journals.rb
@@ -2,6 +2,9 @@ class CreateJournals < ActiveRecord::Migration
   def change
     create_table(:journals) do |t|
       t.string :content
+      t.string :title
+      t.boolean :goal, default: false
+
       t.belongs_to :user
 
       t.timestamp
