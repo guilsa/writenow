4eb7b36e998728d561a16e9649485fd5a2641e28
Guilherme Sá <guilsa001@gmail.com>
2015-05-15 15:52:52 -0700

    add new font to dashboard

--- config/environment.rb
+++ config/environment.rb
@@ -23,6 +23,8 @@ require 'erb'
 require 'pry' if development? || test?
 require 'ffaker' if development? || test?
 
+require 'Date'
+
 # Some helper constants for path-centric logic
 APP_ROOT = Pathname.new(File.expand_path('../../', __FILE__))
 
